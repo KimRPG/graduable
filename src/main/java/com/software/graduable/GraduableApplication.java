ce3a1f69e1a6144f789f6b6328c03c0d295be155
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-09 16:42:04 +0900

    init : 초기설정

---
+++ src/main/java/com/software/graduable/GraduableApplication.java
@@ -0,0 +1,13 @@
+package com.software.graduable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduableApplication.class, args);
+	}
+
+}
