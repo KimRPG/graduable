572dd971128ec166372c66f32e793c1a0af3fec4
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-15 14:22:29 +0900

    :sparkles: : 유효한 값 확인을 위한 Enum 생성

---
+++ src/main/java/com/software/graduable/grade/Category.java
@@ -0,0 +1,31 @@
+package com.software.graduable.grade;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum Category {
+    LIBERAL_COMPULSORY("교필"),
+    LIBERAL_ELECTIVE_COMPULSORY("교선필"),
+    LIBERAL_ELECTIVE("교선"),
+    MAJOR_ELECTIVE("전선"),
+    MAJOR_COMPULSORY("전필"),
+    MAJOR_ELECTIVE_COMPULSORY("전선필");
+
+    final private String status;
+    Category(String status) {
+        this.status = status;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+
+    public static Category nameOf(String name) {
+        for (Category status : Category.values()) {
+            if (status.getStatus().equals(name)) {
+                return status;
+            }
+        }
+        return null;
+    }
+}
