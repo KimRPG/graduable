56e5a0a94d8c48b8188cc590025690c38f6add03
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-16 22:07:31 +0900

    :sparkles: : generate GradeDTO

---
+++ src/main/java/com/software/graduable/grade/GradeDTO.java
@@ -0,0 +1,11 @@
+package com.software.graduable.grade;
+
+public class GradeDTO {
+    private Category classification;
+    private Integer yearCourseTaken;
+    private Integer semesterCourseTaken;
+    private String courseCode;
+    private String courseName;
+    private Credit credit;
+    private String subjectNote;
+}
