d7cd6f3999af0351c6b6aead4bc40e2ebde97648
Giyoul <cla12093@gmail.com>
2025-04-18 11:21:03 +0900

    :sparkles: Add User Entity

--- src/main/java/com/software/graduable/user/User.java
+++ src/main/java/com/software/graduable/user/User.java
@@ -1,2 +1,32 @@
-package com.software.graduable.user;public class User {
+package com.software.graduable.user;
+
+import com.software.graduable.plannedCourse.PlannedCourse;
+import com.software.graduable.userGrade.UserGrade;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<UserGrade> gradeList;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<PlannedCourse> plannedCourseList;
+
+    private String userName;
+    private String userNickname;
+    private String userSemester;
+
 }
