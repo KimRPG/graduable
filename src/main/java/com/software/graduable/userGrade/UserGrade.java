90d163ebad65f9c662aa4eed6e4cd6665392e601
Giyoul <cla12093@gmail.com>
2025-04-19 11:21:03 +0900

    :sparkles: Add UserGrade Entity

--- src/main/java/com/software/graduable/userGrade/UserGrade.java
+++ src/main/java/com/software/graduable/userGrade/UserGrade.java
@@ -1,2 +1,28 @@
-package com.software.graduable.userGrade;public class UserGrade {
+package com.software.graduable.userGrade;
+
+import com.software.graduable.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserGrade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userGradeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_Id")
+    private User user;
+
+    private Long courseId;
+    private Long yearCourseTaken;
+    private Long semesterCourseTaken;
+    private String gradeStatus;
+    private String gradePoint;
+    private String graduateCategory;
 }
