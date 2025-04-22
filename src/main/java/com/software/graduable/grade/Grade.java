1686d6e276ff2f3f5c4bd77ef2866f89a6223d0e
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-22 13:49:05 +0900

    :white_check_mark: 학점 point 추가 + P 추가  + JSONFormat 추가

--- src/main/java/com/software/graduable/grade/Grade.java
+++ src/main/java/com/software/graduable/grade/Grade.java
@@ -3,24 +3,30 @@ package com.software.graduable.grade;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
+
 @Getter
-public enum Credit {
-    A_PLUS("A+"),
-    A("A"),
-    B_PLUS("B+"),
-    B("B"),
-    C_PLUS("C+"),
-    C("C"),
-    D_PLUS("D+"),
-    D("D"),
-    F("F");
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum Grade {
+    A_PLUS("A+", 4.5),
+    A("A0", 4.0),
+    B_PLUS("B+", 3.5),
+    B("B0", 3.0),
+    C_PLUS("C+", 2.5),
+    C("C0", 2.0),
+    D_PLUS("D+", 1.5),
+    D("D0", 1.0),
+    F("F", 0),
+    P("P", 0),
+    ;
 
     final private String status;
-    Credit(String status) {
+    final private double point;
+    Grade(String status, double point) {
         this.status = status;
+        this.point = point;
     }
-    public static Credit nameOf(String name) {
-        for (Credit status : Credit.values()) {
+    public static Grade nameOf(String name) {
+        for (Grade status : Grade.values()) {
             if (status.getStatus().equals(name)) {
                 return status;
             }
