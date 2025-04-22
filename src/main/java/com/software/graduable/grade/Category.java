46042c9bc83d911b30fb71ce6cbae68b4a8fac3b
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-22 13:48:15 +0900

    :white_check_mark: : jsonValue 고침

--- src/main/java/com/software/graduable/grade/Category.java
+++ src/main/java/com/software/graduable/grade/Category.java
@@ -1,10 +1,9 @@
 package com.software.graduable.grade;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @Getter
 public enum Category {
     LIBERAL_COMPULSORY("교필"),
@@ -19,6 +18,10 @@ public enum Category {
         this.status = status;
     }
 
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
     public static Category nameOf(String name) {
         for (Category status : Category.values()) {
             if (status.getStatus().equals(name)) {
