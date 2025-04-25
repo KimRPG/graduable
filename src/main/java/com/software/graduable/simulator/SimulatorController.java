6c11f51f7508ecf630d2435d7e4c24d9ed4744e5
Giyoul <cla12093@gmail.com>
2025-04-25 10:21:03 +0900

    :sparkles: Add simulation controll

--- src/main/java/com/software/graduable/simulator/SimulatorController.java
+++ src/main/java/com/software/graduable/simulator/SimulatorController.java
@@ -3,10 +3,7 @@ package com.software.graduable.simulator;
 //import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +13,14 @@ public class SimulatorController {
 
     @GetMapping("/search")
 //    @Operation(summary = "과목 정보 조회하기 기능", description = "name으로 과목 이름을 보내주면 해당 과목의 정보를 받습니다.")
-    public ResponseEntity<SimulatorDto.response.search> findSubjectByName(@RequestParam String name) {
+    public ResponseEntity<?> findSubjectByName(@RequestParam String name) {
         SimulatorDto.response.search response = simulatorService.findSubjectByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/graduateSimulation")
+    public ResponseEntity<?> graduateSimulation(@RequestBody SimulatorDto.request.graduateSimulation request){
+        SimulatorDto.response.graduateSimulation response = simulatorService.graduateSimulation(request);
         return ResponseEntity.ok(response);
     }
 }
