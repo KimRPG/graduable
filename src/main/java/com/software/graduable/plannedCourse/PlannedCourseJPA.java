3ef327ccb44f0f3a75f0bb16977a9f4da153dee7
Giyoul <cla12093@gmail.com>
2025-04-28 13:11:03 +0900

    :white_check_mark: Add PlannedCourse repo

---
+++ src/main/java/com/software/graduable/plannedCourse/PlannedCourseJPA.java
@@ -0,0 +1,6 @@
+package com.software.graduable.plannedCourse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlannedCourseJPA extends JpaRepository<PlannedCourse, Long> {
+}
