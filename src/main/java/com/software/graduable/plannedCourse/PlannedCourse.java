b0b1265f8938d1d112fb93460f81dba318d437ae
Giyoul <cla12093@gmail.com>
2025-04-29 13:11:03 +0900

    :white_check_mark: Update PlannedCourse Entity

--- src/main/java/com/software/graduable/plannedCourse/PlannedCourse.java
+++ src/main/java/com/software/graduable/plannedCourse/PlannedCourse.java
@@ -22,4 +22,10 @@ public class PlannedCourse {
 
     private String semester;    // 해당 과목이 로드맵에서 몇 학기에 들어가는 과목인지
     private Long courseId;  // 해당 과목 리스트에 들어갈 과목 id값
+
+    public PlannedCourse(User user, String semester, Long courseId) {
+        this.user = user;
+        this.semester = semester;
+        this.courseId = courseId;
+    }
 }
