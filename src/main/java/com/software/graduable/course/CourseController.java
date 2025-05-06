37ccdbc6659acb4d22299598dff06dc7f87b9a50
Giyoul <cla12093@gmail.com>
2025-05-06 13:41:13 +0900

    :sparkles: Add Search All Course Function

---
+++ src/main/java/com/software/graduable/course/CourseController.java
@@ -0,0 +1,22 @@
+package com.software.graduable.course;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("course")
+public class CourseController {
+    private final CourseService courseService;
+
+    @GetMapping("")
+    public ResponseEntity<?> findAllCourses(){
+        List<Course> response = courseService.findAll();
+        return ResponseEntity.ok(response);
+    }
+}
