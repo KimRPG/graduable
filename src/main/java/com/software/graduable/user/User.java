c8474de64e034d0b51b258c30844ca3eee0370b9
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-05-16 21:09:42 +0900

    ✅ Grade 관련 파일 수정 및 테스트 코드 업데이트

--- src/main/java/com/software/graduable/user/User.java
+++ src/main/java/com/software/graduable/user/User.java
@@ -1,8 +1,8 @@
 package com.software.graduable.user;
 
+import com.software.graduable.grade.GradeEntity;
 import com.software.graduable.plannedCourse.PlannedCourse;
 import com.software.graduable.user.dto.UserDTO;
-import com.software.graduable.userGrade.UserGrade;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,21 +23,21 @@ public class User {
     private Long userId;
 
     @OneToMany(mappedBy = "user", orphanRemoval = true)
-    private List<UserGrade> gradeList;  // 유저가 받은 성적 리스트
+    private List<GradeEntity> gradeList;  // 유저가 받은 성적 리스트
 
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     private List<PlannedCourse> plannedCourseList;  // 유저가 학기별 로드맵에 넣어놓은 과목들 리스트
 
     private String userName;    // 유저 이름
     private String userNickname;    // 유저 닉네임
-    private String userSemester;    // 유저 현 학기 수
+    private int userSemester;    // 유저 현 학기 수
     private String googleId;
     private String email;
 
     public User toEntity(UserDTO dto) {
         return User.builder()
                 .userName(dto.getUserName())
-                .userName(dto.getUserNickname())
+                .userNickname(dto.getUserNickname())
                 .userSemester(dto.getUserSemester())
                 .googleId(dto.getGoogleId())
                 .email(dto.getEmail())
