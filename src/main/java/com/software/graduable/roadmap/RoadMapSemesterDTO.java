350cfb2303330d023b1844fde67676b608532b08
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-05-20 00:06:30 +0900

    ✅  long value 수정

--- src/main/java/com/software/graduable/roadmap/RoadMapSemesterDTO.java
+++ src/main/java/com/software/graduable/roadmap/RoadMapSemesterDTO.java
@@ -29,7 +29,7 @@ public class RoadMapSemesterDTO {
     }
 
     public static RoadMapSemesterDTO toDto(Course course, User user, PlannedCourse plannedCourse) {
-        int remain = user.getUserSemester() - Integer.parseInt(plannedCourse.getSemester());
+        int remain = user.getUserSemester() - plannedCourse.getSemester().intValue();
         int year = 2025 + ((remain) / 2);
         int semester = remain % 2 != 1 ? 1 : 2;
         String yearAndSemester = year + "-" + semester;
